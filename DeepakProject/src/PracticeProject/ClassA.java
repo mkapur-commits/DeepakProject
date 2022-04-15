b170f26b657dd6591e916473bc99212d4d1e913c
MK <kapurmeenakshi@hotmail.com>
2022-04-15 07:42:38 -0700

    Commit

---
+++ DeepakProject/src/PracticeProject/ClassA.java
@@ -0,0 +1,13 @@
+package PracticeProject;
+
+public class ClassA {
+
+	int a=20;
+	
+	public static void main(String[] args) {
+		
+		ClassA obj = new ClassA();
+		System.out.println(obj.a);
+	}
+
+}
